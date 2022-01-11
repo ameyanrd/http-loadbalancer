04f9a6335afa2deec92c8809aa620635f05011e8
Shashank D <shashankindiamanoj@gmail.com>
2022-01-12 00:23:10 +0530

    Add a threadsafe roundrobin backend chooser

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,59 +1,108 @@
 import json
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from http.client import parse_headers
+from threading import Lock
 
-from http.server import BaseHTTPRequestHandler, HTTPServer, ThreadingHTTPServer
 import requests
-# from httplib import HTTPResponse
-from os import curdir,sep
+
+
+class ThreadSafeIncrementer:
+    def __init__(self, num_backends):
+        self.value = 0
+        self.num_backends = num_backends
+        self._lock = Lock()
+
+    def nextindex(self):
+        with self._lock:
+            self.value = (self.value + 1) % self.num_backends
+            return self.value
+
+
+global_current_backend = 0
+
 
 class RequestHandler(BaseHTTPRequestHandler):
-
-    protocol_version = 'HTTP/1.0'
-
     def do_GET(self):
-        # Parse request
-        b=open('backend.json')
-        backend= json.load(b)
 
-        url = 'http://{}{}'.format(backend["backends"][0], self.path)
-        req_header = self.parse_headers()
-        
-        # Call the target service
-        resp = requests.get(url, headers=req_header, verify=False)
+        is_request_healthy = False
+        num_backends_tried = 0
 
-        # Respond with the requested data
-        self.send_response(resp.status_code)
-        self.send_resp_headers(resp)
-        self.wfile.write(resp.content)
+        while not is_request_healthy and num_backends_tried < NUM_BACKENDS:
+
+            # We pick a backend here
+            idx = global_current_backend.nextindex()
+
+            url = f"http://{backends[idx]}/{self.path}"
+            req_header = self.parse_headers()
+
+            try:
+                resp = requests.get(url, headers=req_header, verify=False, timeout=3)
+            except requests.ConnectTimeout or requests.exceptions.ReadTimeout as e:
+                self.send_response(504)
+                self.wfile.write("Upstream timed out".encode("utf-8"))
+                return
+
+            print("UPSTREAM STATUS:", resp.status_code)
+            is_request_healthy = True
+            if resp.status_code // 100 == 5:
+                is_request_healthy = False
+                num_backends_tried += 1
+
+        if num_backends_tried >= NUM_BACKENDS:
+            self.send_response(500)
+            self.send_resp_headers(resp)
+            self.wfile.write("No backend available".encode("utf-8"))
+        else:
+            self.send_response(resp.status_code)
+            self.send_resp_headers(resp)
+            self.wfile.write(resp.content)
+
         return
 
     def parse_headers(self):
         req_header = {}
         for line in self.headers:
-            line_parts = [o.strip() for o in line.split(':', 1)]
+            line_parts = [o.strip() for o in line.split(":", 1)]
             if len(line_parts) == 2:
                 req_header[line_parts[0]] = line_parts[1]
         return req_header
 
     def send_resp_headers(self, resp):
         respheaders = resp.headers
-        print ('Response Header')
-        for key in respheaders:
-            if key not in ['Content-Encoding', 'Transfer-Encoding', 'content-encoding', 'transfer-encoding', 'content-length', 'Content-Length']:
-                print (key, respheaders[key])
-                self.send_header(key, respheaders[key])
-        self.send_header('Content-Length', len(resp.content))
+        for header_name in respheaders:
+            if header_name not in [
+                "Content-Encoding",
+                "Transfer-Encoding",
+                "content-encoding",
+                "transfer-encoding",
+                "content-length",
+                "Content-Length",
+                "Connection",
+            ]:
+                self.send_header(header_name, respheaders[header_name])
+        self.send_header("Content-Length", len(resp.content))
         self.end_headers()
 
+
 def run():
-    print('http server is starting...')
-    #by default http server port is 80
-    server_address = ('127.0.0.1', 8080)
+    LISTEN_ADDR = "0.0.0.0"
+    LISTEN_PORT = 3000
+    print("Starting HTTP Listener")
+    server_address = (LISTEN_ADDR, LISTEN_PORT)
     httpd = ThreadingHTTPServer(server_address, RequestHandler)
     try:
-        print('http server is running...')
+        print(f"Listening for connections at http://{LISTEN_ADDR}:{LISTEN_PORT}/")
         httpd.serve_forever()
     except KeyboardInterrupt:
         httpd.socket.close()
 
-if __name__ == '__main__':
+
+NUM_BACKENDS = 0
+
+
+if __name__ == "__main__":
+    with open("backend.json") as f:
+        backends = json.load(f)["backends"]
+    NUM_BACKENDS = len(backends)
+    global_current_backend = ThreadSafeIncrementer(NUM_BACKENDS)
     run()
